30d6cab6dd66c1904cff148eb924361e8372de15
Manu Minnig <89226974+Manuminnig@users.noreply.github.com>
2024-05-13 11:24:33 -0300

    Update request.service.spec.ts

--- src/app/services/request.service.spec.ts
+++ src/app/services/request.service.spec.ts
@@ -1,24 +1,13 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { MatDialog } from '@angular/material/dialog';
-import { of } from 'rxjs';
 
 import { RequestService } from './request.service';
 
 describe('RequestService', () => {
   let service: RequestService;
-  let matDialogMock: MatDialog;
-  
-   beforeEach(() => {
-    matDialogMock = jasmine.createSpyObj('MatDialog', ['open']);
 
-    TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-      providers: [
-        { provide: MatDialog, useValue: matDialogMock }
-      ]
-    });
-     service = TestBed.inject(RequestService);
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(RequestService);
   });
 
   it('should be created', () => {
